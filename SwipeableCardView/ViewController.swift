e03c4a11d8c62b20c516c85ede6446e23a087942
Rahul Chopra <rahulchopra@rahuls-macbook-air.local>
2018-10-16 22:38:33 +0530

    - add swipeable view project

---
+++ SwipeableCardView/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  SwipeableCardView
+//
+//  Created by Rahul Chopra on 15/10/18.
+//  Copyright © 2018 Rahul Chopra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var movedView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+
+    @IBAction func viewPanGestureAction(_ sender: UIPanGestureRecognizer) {
+        let point = sender.translation(in: movedView)
+        let xFromCenter = movedView.center.x - view.center.x
+        movedView.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
+
+        if xFromCenter > 0 {
+            //Moving in +ve direction
+            imageView.image = UIImage(named: "thumb-up")
+            imageView.tintColor = UIColor.green
+        }
+        else {
+            //Moving in -ve direction
+            imageView.image = UIImage(named: "thumb-down")
+            imageView.tintColor = UIColor.red
+        }
+        
+        imageView.alpha = abs(xFromCenter) / view.center.x
+        
+        if sender.state == UIGestureRecognizer.State.ended {
+            
+            if movedView.center.x < 75 {
+                // Move off to the left side
+                UIView.animate(withDuration: 0.4) {
+                    self.movedView.center = CGPoint(x: self.movedView.center.x - 200, y: self.movedView.center.y + 75)
+                    self.movedView.alpha = 0
+                }
+                return
+            }
+            else if movedView.center.x > (self.view.frame.width - 75) {
+                // Move off to the right side
+                UIView.animate(withDuration: 0.4) {
+                    self.movedView.center = CGPoint(x: self.movedView.center.x + 200, y: self.movedView.center.y + 75)
+                    self.movedView.alpha = 0
+                }
+                return
+            }
+            
+            resetCard()
+        }
+    }
+    
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        resetCard()
+    }
+    
+    func resetCard() {
+        UIView.animate(withDuration: 0.2) {
+            self.movedView.center = self.view.center
+            self.imageView.alpha = 0
+            self.movedView.alpha = 1
+        }
+    }
+}
+
